89fd16e5a8d0b989baad4d7bf0220dd92f331c36
chrislo27 <kinchrislo@gmail.com>
2015-09-20 09:31:33 -0700

    Abandoned support for controller (disabled Lstick)

--- core/src/ionium/menutree/MenuTree.java
+++ core/src/ionium/menutree/MenuTree.java
@@ -2,9 +2,15 @@ package ionium.menutree;
 
 import ionium.templates.Main;
 import ionium.util.Utils;
+import ionium.util.controllers.Xbox360Controllers;
+import ionium.util.controllers.Xbox360Mappings;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.controllers.ControllerAdapter;
+import com.badlogic.gdx.controllers.Controllers;
+import com.badlogic.gdx.controllers.PovDirection;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
@@ -25,8 +31,65 @@ public class MenuTree {
 
 	private float x, y;
 	private float indent = 32;
-	
+
 	private Main main;
+
+	private ControllerAdapter controllerAdapter = new ControllerAdapter() {
+
+		@Override
+		public boolean buttonDown(Controller controller, int buttonIndex) {
+			if (controller == Xbox360Controllers.getController(0)) {
+				if(buttonIndex == Xbox360Mappings.BUTTON_A){
+					pressEnter();
+				}else if(buttonIndex == Xbox360Mappings.BUTTON_B || buttonIndex == Xbox360Mappings.BUTTON_BACK){
+					pressBack();
+				}
+			}
+			return true;
+		}
+
+		@Override
+		public boolean buttonUp(Controller controller, int buttonIndex) {
+			return false;
+		}
+
+		@Override
+		public boolean axisMoved(Controller controller, int axisIndex, float value) {
+//			if (controller == Xbox360Controllers.getController(0)) {
+//				if (axisIndex == Xbox360Mappings.AXIS_LX) {
+//					if (value <= -0.75f) {
+//						pressLeft();
+//					} else if (value >= 0.75f) {
+//						pressRight();
+//					}
+//				} else if (axisIndex == Xbox360Mappings.AXIS_LY) {
+//					if (value <= -0.75f) {
+//						pressUp();
+//					} else if (value >= 0.75f) {
+//						pressDown();
+//					}
+//				}
+//			}
+			return false;
+		}
+
+		@Override
+		public boolean povMoved(Controller controller, int povIndex, PovDirection value) {
+			if (controller == Xbox360Controllers.getController(0)
+					&& povIndex == Xbox360Mappings.POV) {
+				if (value == PovDirection.north) {
+					pressUp();
+				} else if (value == PovDirection.south) {
+					pressDown();
+				} else if (value == PovDirection.east) {
+					pressRight();
+				} else if (value == PovDirection.west) {
+					pressLeft();
+				}
+			}
+			return false;
+		}
+	};
 
 	public MenuTree(Main m, float x, float y, float indent) {
 		main = m;
@@ -39,73 +102,119 @@ public class MenuTree {
 		float offsetX = 0;
 		float offsetY = 0;
 
-		// input
+		// keyboard input
 		if (Gdx.input.isKeyJustPressed(Keys.DOWN)) {
-			increaseSelected(1);
+			pressDown();
 		} else if (Gdx.input.isKeyJustPressed(Keys.UP)) {
-			increaseSelected(-1);
+			pressUp();
 		} else if (Gdx.input.isKeyJustPressed(Keys.LEFT)) {
-			if (elements.get(selected).onNextSublevel) {
-				// tell to move left
-				elements.get(selected).moveSublevel(false);
-			}
+			pressLeft();
 		} else if (Gdx.input.isKeyJustPressed(Keys.RIGHT)) {
-			if (elements.get(selected).sublevel.size > 0) {
-				elements.get(selected).moveSublevel(true);
-			}
-		} else if(Gdx.input.isKeyJustPressed(Keys.ENTER) || Gdx.input.isKeyJustPressed(Keys.SPACE)){
-			elements.get(selected).handleEnter();
+			pressRight();
+		} else if (Gdx.input.isKeyJustPressed(Keys.ENTER) || Gdx.input.isKeyJustPressed(Keys.SPACE)) {
+			pressEnter();
+		} else if (Gdx.input.isKeyJustPressed(Keys.BACKSPACE)
+				|| Gdx.input.isKeyJustPressed(Keys.ESCAPE)) {
+			pressBack();
 		}
 
 		renderSublevel(batch, font, offsetX, offsetY, elements, selected, true);
 	}
 
+	protected void pressEnter() {
+		elements.get(selected).handleEnter();
+	}
+
+	protected void pressBack() {
+		pressLeft();
+	}
+
+	protected void pressUp() {
+		increaseSelected(-1);
+	}
+
+	protected void pressDown() {
+		increaseSelected(1);
+	}
+
+	protected void pressLeft() {
+		if (elements.get(selected).onNextSublevel) {
+			// tell to move left
+			elements.get(selected).moveSublevel(false);
+		}
+	}
+
+	protected void pressRight() {
+		if (elements.get(selected).sublevel.size > 0) {
+			elements.get(selected).moveSublevel(true);
+		}
+	}
+
+	public void onScreenShow() {
+		// check to init controllers
+		if (Xbox360Controllers.hasAny()) {
+			Controllers.addListener(controllerAdapter);
+		}
+	}
+
+	public void onScreenHide() {
+		// check to init controllers
+		if (Xbox360Controllers.hasAny()) {
+			Controllers.removeListener(controllerAdapter);
+		}
+	}
+
 	private float renderSublevel(SpriteBatch batch, BitmapFont font, float offsetX, float offsetY,
 			Array<MenuElement> level, int selected, boolean isThisGroupEvenSelected) {
 		boolean shouldBeGreyedOut = false;
-		
+
 		// grey out if group not selected
-		if(!isThisGroupEvenSelected){
+		if (!isThisGroupEvenSelected) {
 			shouldBeGreyedOut = true;
 		}
-		
+
 		// grey out if not already and any members of group are in their sublevel
-		if(!shouldBeGreyedOut){
-			for(MenuElement m : level){
-				if(m.onNextSublevel){
+		if (!shouldBeGreyedOut) {
+			for (MenuElement m : level) {
+				if (m.onNextSublevel) {
 					shouldBeGreyedOut = true;
 					break;
 				}
 			}
 		}
-		
+
 		for (int i = 0; i < level.size; i++) {
 			// draw text
 			font.setColor(1, 1, 1, 1);
-			
-			if(shouldBeGreyedOut){
+
+			if (shouldBeGreyedOut) {
 				font.setColor(0.25f, 0.25f, 0.25f, 1f);
-			}else{
-				if(i == selected){
+			} else {
+				if (i == selected) {
 					font.setColor(0.25f, 0.75f, 1f, 1f);
 				}
 			}
 
-			font.draw(batch, level.get(i).getRenderText(), x * Gdx.graphics.getWidth()
-					+ offsetX, y * Gdx.graphics.getHeight() - offsetY);
-			
-			if(level.get(i).sublevel.size > 0){
+			font.draw(batch, level.get(i).getRenderText(), x * Gdx.graphics.getWidth() + offsetX, y
+					* Gdx.graphics.getHeight() - offsetY);
+
+			if (level.get(i).sublevel.size > 0) {
 				main.font.setColor(0.25f, 0.25f, 0.25f, 1f);
-				main.font.draw(batch, "   >", x * Gdx.graphics.getWidth()
-						+ offsetX + Utils.getWidth(font, level.get(i).getRenderText()), y * Gdx.graphics.getHeight() - offsetY);
+				main.font.draw(
+						batch,
+						"   >",
+						x * Gdx.graphics.getWidth() + offsetX
+								+ Utils.getWidth(font, level.get(i).getRenderText()), y
+								* Gdx.graphics.getHeight() - offsetY);
 				main.font.setColor(1, 1, 1, 1);
 			}
-			
+
 			// show the selected arrow if the menu is:
 			//    - selected, the group selected, and not on the next sublevel
-			if(i == selected && isThisGroupEvenSelected && !level.get(i).onNextSublevel){
-				font.draw(batch, "> ", x * Gdx.graphics.getWidth()
-						+ offsetX - Utils.getWidth(font, "> "), y * Gdx.graphics.getHeight() - offsetY);
+			if (i == selected && isThisGroupEvenSelected && !level.get(i).onNextSublevel) {
+				font.draw(batch, "> ",
+						x * Gdx.graphics.getWidth() + offsetX - Utils.getWidth(font, "> "), y
+								* Gdx.graphics.getHeight() - offsetY);
 			}
 
 			// increase Y offset
@@ -114,7 +223,8 @@ public class MenuTree {
 			// find sublevels, recurse through them and render
 			if (level.get(i).sublevel.size > 0 && i == selected && isThisGroupEvenSelected) {
 				offsetY = renderSublevel(batch, font, offsetX + indent, offsetY,
-						level.get(i).sublevel, level.get(i).getSelected(), level.get(i).onNextSublevel);
+						level.get(i).sublevel, level.get(i).getSelected(),
+						level.get(i).onNextSublevel);
 			}
 		}
 
