ba1b94ade0af25d6b8ea2b5c04c5fdadb79b2fb0
chrislo27 <kinchrislo@gmail.com>
2015-09-25 21:21:56 -0700

    Added debug key constant to DebugSettings

--- core/src/ionium/util/DebugSetting.java
+++ core/src/ionium/util/DebugSetting.java
@@ -1,8 +1,12 @@
 package ionium.util;
 
+import com.badlogic.gdx.Input.Keys;
+
 
 public class DebugSetting {
 
+	public static final int DEBUG_KEY = Keys.F8;
+	
 	public static boolean debug = false;
 	public static boolean showFPS = true;
 	
