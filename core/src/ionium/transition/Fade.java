b99c3f960ec76823e104917c82d4952a1cbba2fd
chrislo27 <kinchrislo@gmail.com>
2015-10-03 17:13:37 -0700

    Removed FadeIn/FadeOut, replaced with a common Fade class

---
+++ core/src/ionium/transition/Fade.java
@@ -0,0 +1,46 @@
+package ionium.transition;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+
+import ionium.templates.Main;
+
+
+public class Fade implements Transition{
+
+	private static final Color REUSED = new Color(0, 0, 0, 1);
+	
+	float timeLeft = 1f;
+	int color = Color.rgba8888(Color.BLACK);
+	boolean fadingOut = true;
+	
+	public Fade(boolean fadingOut, int color, float time){
+		this.fadingOut = fadingOut;
+		this.color = color;
+		timeLeft = time;
+	}
+	
+	@Override
+	public boolean finished() {
+		return timeLeft <= 0;
+	}
+
+	@Override
+	public void render(Main main) {
+		REUSED.set(color);
+		
+		main.batch.setColor(REUSED.r, REUSED.g, REUSED.b, (fadingOut == false ? -1 : 0) + Math.min(timeLeft, 1f));
+		Main.fillRect(main.batch, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		main.batch.setColor(1, 1, 1, 1);
+		
+		if (timeLeft > 0) {
+			timeLeft -= Gdx.graphics.getDeltaTime();
+			if (timeLeft < 0) timeLeft = 0;
+		}
+	}
+
+	@Override
+	public void tickUpdate(Main main) {
+	}
+
+}
