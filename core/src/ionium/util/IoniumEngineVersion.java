d2eacddfcf8d68372534138befafb95dc2910b73
chrislo27 <kinchrislo@gmail.com>
2015-10-11 13:53:01 -0700

    Elevate version to v1.1.0

--- core/src/ionium/util/IoniumEngineVersion.java
+++ core/src/ionium/util/IoniumEngineVersion.java
@@ -3,7 +3,7 @@ package ionium.util;
 
 public final class IoniumEngineVersion {
 
-	public static final String ENGINE_VERSION = "v1.0.1";
+	public static final String ENGINE_VERSION = "v1.1.0";
 	
 	private IoniumEngineVersion(){
 		
