1a0a18b3536307135b270da3fe95cc96b0ca32b2
chrislo27 <kinchrislo@gmail.com>
2015-11-06 15:20:21 -0800

    Resolution determination uses desktop display mode

--- core/src/ionium/util/resolution/ResolutionDeterminator.java
+++ core/src/ionium/util/resolution/ResolutionDeterminator.java
@@ -1,10 +1,12 @@
 package ionium.util.resolution;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Comparator;
 
 import com.badlogic.gdx.Gdx;
+
+import ionium.templates.Main;
 
 /**
  * Helps determine resolution.
@@ -18,7 +20,8 @@ public class ResolutionDeterminator {
 	 * @param config
 	 * @param possibleResolutions list of resolutions to use, will be sorted by size
 	 */
-	public static void determineIdealResolution(Resolutable config, Resolution[] possibleResolutions) {
+	public static void determineIdealResolution(Resolutable config,
+			Resolution[] possibleResolutions) {
 		ArrayList<Resolution> sorted = new ArrayList<>();
 
 		for (Resolution r : possibleResolutions) {
@@ -33,22 +36,25 @@ public class ResolutionDeterminator {
 			}
 
 		});
-		
-		for(Resolution r : sorted){
-			if(doesResolutionFit(r)){
+
+		for (int i = sorted.size() - 1; i >= 0; i--) {
+			Resolution r = sorted.get(i);
+
+			if (doesResolutionFit(r)) {
 				config.setWidth(r.width);
 				config.setHeight(r.height);
 				break;
 			}
 		}
 	}
-	
-	public static boolean doesResolutionFit(Resolution r){
-		
-		if(r.width > Gdx.app.getGraphics().getWidth() || r.height > Gdx.app.getGraphics().getHeight()){
+
+	public static boolean doesResolutionFit(Resolution r) {
+
+		if (r.width > Gdx.graphics.getDesktopDisplayMode().width
+				|| r.height > Gdx.graphics.getDesktopDisplayMode().height) {
 			return false;
 		}
-		
+
 		return true;
 	}
 
