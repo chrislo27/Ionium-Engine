31ca0ac982ec13973017b57380acc8ff5a436ef8
chrislo27 <kinchrislo@gmail.com>
2015-12-18 17:42:29 -0800

    Stores last null screen id

--- core/src/ionium/registry/ScreenRegistry.java
+++ core/src/ionium/registry/ScreenRegistry.java
@@ -26,6 +26,8 @@ public class ScreenRegistry implements Disposable{
 	private HashMap<String, Updateable> updateables = new HashMap<>(16);
 	private Array<Updateable> all = new Array<>(16);
 	
+	public String lastNullScreen = "";
+	
 	private void loadResources() {
 
 	}
@@ -37,11 +39,25 @@ public class ScreenRegistry implements Disposable{
 		return this;
 	}
 	
+	private static boolean checkIfScreenDoesNotExist(String id){
+		if(instance().updateables.get(id) == null){
+			instance().lastNullScreen = id;
+			
+			return true;
+		}
+		
+		return false;
+	}
+	
 	public static Updateable get(String id){
+		checkIfScreenDoesNotExist(id);
+		
 		return instance().updateables.get(id);
 	}
 	
 	public static <T extends Updateable> T get(String id, Class<T> cls){
+		checkIfScreenDoesNotExist(id);
+		
 		return cls.cast(get(id));
 	}
 	
