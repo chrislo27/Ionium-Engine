651ba44539cb7d3b2b210e6edd0932abe12f8c91
chrislo27 <kinchrislo@gmail.com>
2016-03-01 15:59:22 -0800

    Renamed

--- core/src/ionium/runnables/AudioChangePitch.java
+++ core/src/ionium/runnables/AudioChangePitch.java
@@ -2,12 +2,12 @@ package ionium.runnables;
 
 import com.badlogic.gdx.audio.Music;
 
-public class AudioPitchChange implements Runnable {
+public class AudioChangePitch implements Runnable {
 
 	public final Music mus;
 	public final float pitch;
 	
-	public AudioPitchChange(Music mus, float pitch) {
+	public AudioChangePitch(Music mus, float pitch) {
 		this.mus = mus;
 		this.pitch = pitch;
 	}
