5c962ad5f044ea71a88bfca7019bc343c5aae4f1
chrislo27 <kinchrislo@gmail.com>
2016-04-16 12:01:16 -0700

    Changed readability

--- core/src/ionium/util/render/TextureRegionDebleeder.java
+++ core/src/ionium/util/render/TextureRegionDebleeder.java
@@ -4,17 +4,18 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class TextureRegionDebleeder {
 
-	public static float fixAmount = 0.01f;
+	public static float fixAmountPx = 0.01f;
 
 	public static void fixBleeding(TextureRegion region) {
 		float x = region.getRegionX();
 		float y = region.getRegionY();
 		float width = region.getRegionWidth();
 		float height = region.getRegionHeight();
-		float invTexWidth = 1f / region.getTexture().getWidth();
-		float invTexHeight = 1f / region.getTexture().getHeight();
-		region.setRegion((x + fixAmount) * invTexWidth, (y + fixAmount) * invTexHeight,
-				(x + width - fixAmount) * invTexWidth, (y + height - fixAmount) * invTexHeight);
+
+		region.setRegion((x + fixAmountPx) / region.getTexture().getWidth(),
+				(y + fixAmountPx) / region.getTexture().getHeight(),
+				(x + width - fixAmountPx) / region.getTexture().getWidth(),
+				(y + height - fixAmountPx) / region.getTexture().getHeight());
 	}
 
 }
