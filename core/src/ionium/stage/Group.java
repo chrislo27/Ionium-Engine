6c9b16e6571894e8d94d122ae08e94efacb705ee
chrislo27 <kinchrislo@gmail.com>
2016-05-10 15:51:43 -0700

    Move stage stuff to stage package

--- core/src/ionium/stage/Group.java
+++ core/src/ionium/stage/Group.java
@@ -1,4 +1,4 @@
-package ionium.ui;
+package ionium.stage;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Array;
--- core/src/ionium/stage/Stage.java
+++ core/src/ionium/stage/Stage.java
@@ -1,4 +1,4 @@
-package ionium.ui;
+package ionium.stage;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
