5b9c22b6b2fea24aaab0db44eff62264e6a59728
chrislo27 <kinchrislo@gmail.com>
2016-05-11 16:17:10 -0700

    Stopped loading old UI assets

--- core/src/ionium/registry/handler/StockAssetLoader.java
+++ core/src/ionium/registry/handler/StockAssetLoader.java
@@ -14,23 +14,6 @@ public class StockAssetLoader implements IAssetLoader {
 
 	@Override
 	public void addManagedAssets(AssetManager manager) {
-		// ui
-		manager.load(AssetMap.add("guilanguage", "images/ui/button/language.png"), Texture.class);
-		manager.load(AssetMap.add("guisettings", "images/ui/button/settings.png"), Texture.class);
-		manager.load(AssetMap.add("guibg", "images/ui/button/bg.png"), Texture.class);
-		manager.load(AssetMap.add("guibgtext", "images/ui/button/bg-textfield.png"), Texture.class);
-		manager.load(AssetMap.add("guislider", "images/ui/button/slider.png"), Texture.class);
-		manager.load(AssetMap.add("guisliderarrow", "images/ui/button/sliderarrow.png"),
-				Texture.class);
-		manager.load(AssetMap.add("guiexit", "images/ui/button/exit.png"), Texture.class);
-		manager.load(AssetMap.add("guiback", "images/ui/button/backbutton.png"), Texture.class);
-		manager.load(AssetMap.add("guibgfalse", "images/ui/button/bgfalse.png"), Texture.class);
-		manager.load(AssetMap.add("guibgtrue", "images/ui/button/bgtrue.png"), Texture.class);
-		
-		// sfx
-
-		// music
-
 	}
 
 	@Override
