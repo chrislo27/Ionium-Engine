094fd92688a9340811c377c4037d96a52967f2c3
chrislo27 <kinchrislo@gmail.com>
2016-05-16 17:20:17 -0700

    Default resolution is 720p windowed

--- core/src/ionium/registry/GlobalVariables.java
+++ core/src/ionium/registry/GlobalVariables.java
@@ -6,8 +6,9 @@ import com.badlogic.gdx.Preferences;
 
 public class GlobalVariables {
 
-	public static int defaultWidth = 1920;
-	public static int defaultHeight = 1080;
+	public static int defaultWidth = 1280;
+	public static int defaultHeight = 720;
+	public static boolean defaultFullscreen = false;
 	public static int ticks = 20;
 	public static int maxFps = 60;
 	public static String versionUrl = null;
